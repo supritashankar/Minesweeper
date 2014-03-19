fe9580fb5f537d6c206165cfed45e9385b2e1414
Suprita Shankar <suprita.shankar@gmail.com>
2014-03-19 16:32:34 -0400

    tidy

--- sweeper/mines/urls.py
+++ sweeper/mines/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('mines.views',
-    url(r'^randomize/(?P<n>[3|6|9])+','randomize'),
+    url(r'^randomize/(?P<num_cells>[0-9]+)/(?P<num_mines>[0-9]+)/$','randomize'),
 )
--- sweeper/mines/views.py
+++ sweeper/mines/views.py
@@ -1,15 +1,16 @@
 import json
 import random
+from random import shuffle
 
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def randomize(request, num_cells, num_mines):
-  grid = [1]*num_cells
-  mines = [[i] for i in range(num_cells)]
+  grid = [1]*int(num_cells)
+  mines = [[i] for i in range(0,int(num_cells))]
   shuffle(mines)
-  mines = mines[:10]
+  mines = mines[:int(num_mines)]
   for i in mines:
-    grid[i] = 0
+    grid[i[0]] = 0
   message = {'grid' : grid}
   return HttpResponse(json.dumps(message), mimetype = 'application/json')   
