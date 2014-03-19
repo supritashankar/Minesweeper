83c02c4c2d513b2d760ffa0305731747de7dbe5b
Suprita Shankar <suprita.shankar@gmail.com>
2014-03-19 01:52:25 -0400

    more changes

--- sweeper/frontend/urls.py
+++ sweeper/frontend/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('frontend.views',
     url(r'^newgame','newgame'),
     url(r'^(?P<level>B|I|E)/$','display_mine'),
-    url(r'^(?P<level>B|I|E)/steps/(?P<index>[0-9]+)/$', 'game')
+    url(r'^steps/$', 'game')
 )
--- sweeper/frontend/views.py
+++ sweeper/frontend/views.py
@@ -1,25 +1,39 @@
 import requests
 
 from django.shortcuts import render, render_to_response
+from django.core.context_processors import csrf
 
 def newgame(request):
   return render_to_response('frontend/newgame.html')
 
 def display_mine(request, level):
-  values = {0:1,1:1,2:0,3:1, 4:0, 5:1, 6:1, 7:1, 8:1}
-  response = requests.get('http://127.0.0.1:8000/mines/randomize/3/')
-  print response.content
-  request.session['values'] = values
-  level = 'Beginner'
-  loopcounter = 3
-  return render_to_response('frontend/beg-game.html', locals())
+  c = {}
+  if level == 'B':
+    values = {0:1,1:1,2:0,3:1, 4:0, 5:1, 6:1, 7:1, 8:1}
+    response = requests.get('http://127.0.0.1:8000/mines/randomize/3/')
+    print response.content
+    request.session['values'] = values
+    c = { 'level' : 'Beginner', 'loopcounter': '3', 'values': values}
+    c.update(csrf(request))
 
-def game(request, level, index):
+  if level == 'I':
+    values = {0:1,1:1,2:0,3:1, 4:0, 5:1, 6:1, 7:1, 8:1, 9:0, 10:1, 11:1, 12:1, 13:1, 14:1, 15:1}
+    loopcounter = 4
+    level = 'Intermediate'
+
+  if level == 'E':
+    values = {0:1,1:1,2:0,3:1, 4:0, 5:1, 6:1, 7:1, 8:1, 9:0, 10:1, 11:1, 12:1, 13:1, 14:1, 15:1, 16:0, 17:0, 18:0, 19:0, 20:1, 21:1, 22:1, 22:1, 23:1, 24:1}
+    loopcounter = 5
+    level = 'Expert'
+    
+  return render_to_response('frontend/beg-game.html', c)
+
+def game(request):
+  print request.POST
   values = request.session['values']
-  values = {0:1,1:1,2:-1,3:1}
   for key,value in values.iteritems():
     if index == key:
-      if value == -1:
+      if value == 0:
         return render_to_response('frontend/game-over.html')
 
       count = get_neighbour_count(index, values)
